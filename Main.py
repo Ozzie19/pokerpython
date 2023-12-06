a07feb166134f88db09e472882aa88828bd7d151
Ozzie19 <odorunay@gmail.com>
2023-12-06 22:32:50 +0000

    Comments

--- Main.py
+++ Main.py
@@ -6,60 +6,75 @@ from Deck import Card, Deck
 #print(card)
 
 #requires updating for asynchronous programming
+# The Player class represents a player in the game.
 class Player:
+    # Each player has a name, a number of chips, and a current bet.
     def __init__(self, name, chips):
         self.name = name
         self.chips = chips
         self.current_bet = 0
 
+    # The check method represents the action of checking in poker.
     def check(self):
-        pass #improve - use a check boolean to check if the player has "check"ed for the game management algorithm
+        pass 
 
+    # The bet method allows a player to bet a certain amount of chips.
     def bet(self, amount):
         self.chips -= amount
         self.current_bet = amount
 
+    # The call method allows a player to call the current bet.
     def call(self, amount):
         self.chips -= amount
         self.current_bet = amount
 
+    # The raise_bet method allows a player to raise the current bet.
     def raise_bet(self, amount):
         self.chips -= amount
         self.current_bet += amount
 
+    # The fold method allows a player to fold their hand.
     def fold(self):
         self.current_bet = 0
 
-class BettingRound: #needs method to end betting round 
-        def __init__(self, players):
-            self.pot = 0
-            self.current_bet = 0
-            self.players = players
+# The BettingRound class represents a round of betting in the game.
+class BettingRound: 
+    # Each betting round has a pot, a current bet, and a list of players.
+    def __init__(self, players):
+        self.pot = 0
+        self.current_bet = 0
+        self.players = players
 
-        def play_round(self):
-            for player in self.players:
-                # Here I will add logic to decide the player's action - to be incorporated into future module
-                pass
-
-        def handle_check(self, player):
+    # The play_round method allows each player to take an action.
+    def play_round(self):
+        for player in self.players:
+            # Here I will add logic to decide the player's action - to be incorporated into future module
             pass
 
-        def handle_bet(self, player, amount):
-            player.bet(amount)
-            self.pot += amount
-            self.current_bet = amount
+    # The handle_check method handles a player checking.
+    def handle_check(self, player):
+        pass
 
-        def handle_call(self, player):
-            player.call(self.current_bet)
-            self.pot += self.current_bet
+    # The handle_bet method handles a player betting a certain amount.
+    def handle_bet(self, player, amount):
+        player.bet(amount)
+        self.pot += amount
+        self.current_bet = amount
 
-        def handle_raise(self, player, amount):
-            player.raise_bet(amount)
-            self.pot += player.current_bet
-            self.current_bet = player.current_bet
+    # The handle_call method handles a player calling the current bet.
+    def handle_call(self, player):
+        player.call(self.current_bet)
+        self.pot += self.current_bet
 
-        def handle_fold(self, player):
-            player.fold()
+    # The handle_raise method handles a player raising the current bet.
+    def handle_raise(self, player, amount):
+        player.raise_bet(amount)
+        self.pot += player.current_bet
+        self.current_bet = player.current_bet
+
+    # The handle_fold method handles a player folding their hand.
+    def handle_fold(self, player):
+        player.fold()
 
 def bettingAlgorithmTests():    
     # Create some players
